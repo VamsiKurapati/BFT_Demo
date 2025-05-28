5f44a1c1167ad3f61bbef1d2c3300f69290a25ec
VamsiKurapati <vamsikrishna.k20@iiits.in>
2025-05-28 21:46:04 +0530

    fix

--- src/assets/MyTrips.jsx
+++ src/assets/MyTrips.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useNavigate } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaSearch, FaTimesCircle } from 'react-icons/fa';
 import car_myTrips from "/car-myTrips.png";
 
