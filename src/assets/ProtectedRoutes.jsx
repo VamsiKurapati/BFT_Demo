1086876de4866263089a809eb4ccf9b5612a430f
VamsiKurapati <vamsikrishna.k20@iiits.in>
2025-05-29 18:48:17 +0530

    changes

--- src/assets/ProtectedRoutes.jsx
+++ src/assets/ProtectedRoutes.jsx
@@ -3,6 +3,9 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoutes = ({ children, allowedRoles }) => {
     const {loginDetails} = JSON.parse(localStorage.getItem("loginDetails")) || {};
 
+    console.log("ProtectedRoutes: ", loginDetails);
+    
+    // Check if loginDetails exists and if the user has the allowed role
     if (!loginDetails) {
         return <Navigate to="/login" replace />;
     }
