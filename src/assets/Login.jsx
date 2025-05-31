7bc9c482c6bd065d36096d16b2cce71c920f9cb7
VamsiKurapati <vamsikrishna.k20@iiits.in>
2025-06-01 01:29:44 +0530

    fixed gap issue

--- src/assets/Login.jsx
+++ src/assets/Login.jsx
@@ -100,7 +100,7 @@ const Login = () => {
 
   return (
     <div
-      className="w-full h-full bg-white p-4 flex items-center justify-center"
+      className="max-w-screen min-h-screen bg-white p-4 flex items-center justify-center"
     >
       <div
         className='w-full h-full relative left-0 bg-cover bg-center transition-all duration-500 ease-in-out rounded-xl'
